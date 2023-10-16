8fe003b5b6d9a5fd164b0458f4a151df26d9f052
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2023-10-16 02:44:52 +0200

    yo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="samira chettori"/>
+  <img alt="Vue logo" src="./assets/sam.jpeg">
+  <HelloWorld/>
 </template>
 
 <script>
